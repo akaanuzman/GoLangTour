69ebd72f57ec1bbb7bffc00d2162df841a109aa0
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-07-14 19:49:32 +0300

    init project

---
+++ todo-app/domains/todo.go
@@ -0,0 +1,11 @@
+package domains
+
+type Todo struct {
+	Id          int64
+	Title       string
+	Description string
+	IsDone      bool
+	CreatedAt   string
+	UpdatedAt   string
+	DueDate     string
+}
