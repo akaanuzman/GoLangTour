ffcdb48054cd4ff9dafa5cdcaa364fe88a42085f
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2024-07-27 17:43:28 +0300

    refactor: Update authentication routes and controllers and added note service, controller and routes

--- note-app/src/routes/routes.go
+++ note-app/src/routes/routes.go
@@ -14,6 +14,7 @@ func InitRoutes(e *echo.Echo) {
 	base := e.Group("/api/v1")
 
 	initAuthRoutes(base)
+	initNoteRoutes(base)
 }
 
 // initAuthRoutes initializes the routes for the authentication endpoints.
@@ -29,4 +30,20 @@ func initAuthRoutes(e *echo.Group) {
 	protected := auth.Group("")
 	protected.Use(middlewares.JWTMiddleware())
 	protected.POST("/change-password", authController.ChangePassword)
+}
+
+// initNoteRoutes initializes the routes for the note endpoints.
+func initNoteRoutes(e *echo.Group) {
+	note := e.Group("/notes")
+	noteService := services.NewNoteService()
+	noteController := controllers.NewNoteController(noteService)
+
+	note.Use(middlewares.JWTMiddleware())
+	note.GET("", noteController.GetAllNotes)
+	note.GET("/:id", noteController.GetNoteById)
+	note.GET("/user/:userId", noteController.GetNoteByUserId)
+	note.GET("/tags", noteController.GetNotesByTags)
+	note.POST("", noteController.CreateNote)
+	note.PUT("/:id", noteController.UpdateNote)
+	note.DELETE("/:id", noteController.DeleteNote)
 }
